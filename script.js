8be4b4e55fd299b3520b9956e82319fa068a6136
itsthekentinator <134373636+itsthekentinator@users.noreply.github.com>
2023-08-14 01:37:44 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,10 @@
 // script.js
-document.addEventListener("DOMContentLoaded", function () {
-  const canvas = document.getElementById("illustrationCanvas");
-  const ctx = canvas.getContext("2d");
-
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-
-  // Draw the simplified illustration
-  ctx.fillStyle = "#FFFFFF"; // White color
-  ctx.fillRect(150, 150, 100, 100); // Draw the first person
-  ctx.fillRect(300, 150, 100, 100); // Draw the second person
-});
-
-function goToStart() {
-  window.location.href = "https://www.example.com/start-link"; // Replace with your actual link
+function start() {
+  // Replace this URL with the actual link
+  window.location.href = "https://www.example.com/start-page";
 }
 
-function goToRough() {
-  window.location.href = "https://www.example.com/rough-link"; // Replace with your actual link
+function rough() {
+  // Replace this URL with the actual link
+  window.location.href = "https://www.example.com/rough-page";
 }
