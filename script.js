57fda0cc80a0e0bcf55e505dc7d3eb260e3c05bb
itsthekentinator <134373636+itsthekentinator@users.noreply.github.com>
2023-08-14 11:23:44 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // script.js
 function start() {
-  window.location.href = "https://www.example.com/start-link";
+  window.location.href = "https://music.apple.com/jp/playlist/sex/pl.u-LdbqxEmFxGX9GBG?l=en-US";
 }
 
 function rough() {
